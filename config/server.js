a98b6479400e23f558d08b476ca0283585c3f0f7
yastamardika <rattyasta1@gmail.com>
2020-12-09 19:40:58 +0700

    initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'b115c303b3716e972cc1177cd2a0051c'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'b6138179-9d18-4bd9-9277-e436546621b5'
+};
